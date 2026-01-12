b611371235c65a111a2572b1f56e045752bf3c46
Zabukovec <rok.zabukovec@heureka.group>
2026-01-12 22:18:44 +0100

    Remove legacy message broker implementation and restructure solution

--- SamoBot/MessageBrokerOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace SamoBot;
-
-public class MessageBrokerOptions
-{
-    public const string SectionName = "MessageBroker";
-
-    public string Provider { get; set; } = "Kafka"; // "Kafka" or "RabbitMQ"
-}
