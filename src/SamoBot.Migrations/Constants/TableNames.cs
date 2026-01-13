6f42581bc3320a14e377a69908591a4e84ccc5f4
Zabukovec <rok.zabukovec@heureka.group>
2026-01-13 15:45:38 +0100

    Add `TableNames` constant class for migration support

---
+++ src/SamoBot.Migrations/Constants/TableNames.cs
@@ -0,0 +1,9 @@
+namespace SamoBot.Migrations.Constants;
+
+internal static class TableNames
+{
+    internal static class Database
+    {
+        public const string DiscoveredUrls = "DiscoveredUrls";
+    }
+}
