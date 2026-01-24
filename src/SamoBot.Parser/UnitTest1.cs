1cd8f3553d4dd96ec84786b9d7258041988ae851
Zabukovec <rok.zabukovec@heureka.group>
2026-01-24 22:42:14 +0100

    Add `SamoBot.Parser` project with initial NUnit test setup
    - Include `SamoBot.Parser` project targeting `.NET 10.0` with enabled nullable and implicit usings.
    - Add initial NUnit test infrastructure with a placeholder test class.
    - Update solution file to reference the new project.

---
+++ src/SamoBot.Parser/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SamoBot.Parser;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
